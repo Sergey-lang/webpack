fbaa22d3c09220cfdd89315dccf2433ab3bc8d8e
Siarhei <siarhei.kukharonak@startmatter.com>
2024-06-20 17:11:54 +0300

    add dev server configured server add mods

--- webpack.config.ts
+++ webpack.config.ts
@@ -2,16 +2,23 @@
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import * as path from 'path';
 import webpack from 'webpack';
+import type { Configuration as DevServerConfiguration } from "webpack-dev-server";
+const devServer: DevServerConfiguration = {};
 
 type ModeType = 'production' | 'development'
 
 interface EnvVariables {
-    mode: ModeType
+    mode: ModeType;
+    port: number;
 }
 
 export default (env: EnvVariables) => {
 
     console.log('[Mode]: ', env.mode);
+    console.log('[Port]: ', env.port);
+
+    const isDev = env.mode === 'development';
+    const isProd = env.mode === 'production';
 
     const config: webpack.Configuration = {
         mode: env.mode ?? 'development', // https://webpack.js.org/configuration/mode/#root
@@ -23,8 +30,8 @@ export default (env: EnvVariables) => {
         },
         plugins: [
             new HtmlWebpackPlugin({template: path.resolve(__dirname, 'public', 'index.html')}),// https://webpack.js.org/concepts/#plugins
-            new webpack.ProgressPlugin(),
-        ],
+            isDev && new webpack.ProgressPlugin(),
+        ].filter(Boolean),
         module: {
             rules: [
                 {
@@ -37,6 +44,15 @@ export default (env: EnvVariables) => {
         resolve: {
             extensions: ['.tsx', '.ts', '.js'],
         },
+        devServer: isDev ? {
+            port: env.port ?? 3000,
+            open: true,
+            static: './dist',
+        } : undefined,
+        optimization: {
+            runtimeChunk: 'single',
+        },
+        devtool: isDev && 'inline-source-map', // https://webpack.js.org/guides/development/#using-source-maps
     };
     return config
 }
