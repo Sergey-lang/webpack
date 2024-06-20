a2e1bd68913c86396cec705438a0a726100fe1a6
Siarhei <siarhei.kukharonak@startmatter.com>
2024-06-20 15:13:54 +0300

    add ts loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const webpack = require('webpack');
 
 module.exports = (env) => {
 
@@ -7,14 +8,27 @@ module.exports = (env) => {
 
     return {
         mode: env.mode ?? 'development', // https://webpack.js.org/configuration/mode/#root
-        entry: path.resolve(__dirname, 'src', 'index.js'),
+        entry: path.resolve(__dirname, 'src', 'index.ts'),
         output: {
             filename: '[name].[contenthash].js',
             path: path.resolve(__dirname, 'build'),
             clean: true
         },
         plugins: [
-            new HtmlWebpackPlugin({template: path.resolve(__dirname, 'public', 'index.html')}) // https://webpack.js.org/concepts/#plugins
-        ]
+            new HtmlWebpackPlugin({template: path.resolve(__dirname, 'public', 'index.html')}),// https://webpack.js.org/concepts/#plugins
+            // new webpack.ProgressPlugin(),
+        ],
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/, // https://webpack.js.org/guides/typescript/#loader
+                    use: 'ts-loader',
+                    exclude: /node_modules/,
+                },
+            ],
+        },
+        resolve: {
+            extensions: ['.tsx', '.ts', '.js'],
+        },
     }
 }
