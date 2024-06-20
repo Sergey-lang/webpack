b66203db9484a76816b2782b5e9cbc295e8f37b9
Siarhei <siarhei.kukharonak@startmatter.com>
2024-06-20 14:32:00 +0300

    add template

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,20 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = (env) => {
 
-    console.log('Mode: ', env.mode); // true
+    console.log('[Mode]: ', env.mode);
 
     return {
         mode: env.mode ?? 'development', // https://webpack.js.org/configuration/mode/#root
         entry: path.resolve(__dirname, 'src', 'index.js'),
         output: {
-            filename: '[name].js',
+            filename: '[name].[contenthash].js',
             path: path.resolve(__dirname, 'build'),
             clean: true
         },
+        plugins: [
+            new HtmlWebpackPlugin({template: path.resolve(__dirname, 'public', 'index.html')}) // https://webpack.js.org/concepts/#plugins
+        ]
     }
 }
