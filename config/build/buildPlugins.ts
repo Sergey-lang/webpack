8f61cac7c52f73fdce7004cfd2005cc01ec0dc33
Siarhei <siarhei.kukharonak@startmatter.com>
2024-06-23 12:22:31 +0300

    add favicon add copy plugin

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -5,13 +5,15 @@ import { IBuildOptions } from './types/types';
 import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
 import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
+import path from 'path';
+import CopyPlugin from 'copy-webpack-plugin';
 
 export function buildPlugins({mode, paths, analyzer, platform}: IBuildOptions): Configuration['plugins'] {
     const isDev = mode === 'development';
     const isProd = mode === 'production';
 
     const plugins: Configuration['plugins'] = [
-        new HtmlWebpackPlugin({template: paths.html}),// https://webpack.js.org/concepts/#plugins
+        new HtmlWebpackPlugin({template: paths.html, favicon: path.resolve(paths.public, 'favicon.ico')}),// https://webpack.js.org/concepts/#plugins
         new webpack.DefinePlugin({
             __PLATFORM__: JSON.stringify(platform),
         }), // https://webpack.js.org/plugins/define-plugin/
@@ -28,6 +30,13 @@ export function buildPlugins({mode, paths, analyzer, platform}: IBuildOptions): 
             filename: 'css/[name].[contenthash:8].css',
             chunkFilename: 'css/[name].[contenthash:8].css',
         })) // https://webpack.js.org/plugins/mini-css-extract-plugin/)
+        plugins.push(
+            new CopyPlugin({
+                patterns: [
+                    { from: path.resolve(paths.public, 'locales'), to: path.resolve(paths.output, 'locales') },
+                ],
+            }), // https://webpack.js.org/plugins/copy-webpack-plugin/
+        )
     }
 
     if (analyzer) {
